d7a75e4ef47eb593ea1a9b432733a565b3685935
Patrick Collins <patrickcollins@macmini2.local>
2020-05-02 22:47:00 +1000

    Added cron stuff

---
+++ cron_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Starting processing..."
+date
+
+# Make sure it's not already running
+pgrep -f icloudpd && echo "icloudpd is already running." && exit
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+
+/Users/patrickcollins/software_projects/icloud_photos_downloader/venv/bin/python \
+	/Users/patrickcollins/software_projects/icloud_photos_downloader/icloudpd.py  \
+        -u pcollins1@gmail.com \
+        -p Catjump2 \
+        -d /Volumes/boxeedisk/Google\ Drive/iCloudPhotos \
+        --no-progress-bar \
+        --until-found 20 \
+        --auto-delete
